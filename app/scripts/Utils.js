f5cfa0c01d751ac8b6660dc0792104f7db1a88f8
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2014-05-15 22:29:42 -0700

    Added util support - max and min out of a field; needs support for plain value lists and tests too

---
+++ app/scripts/Utils.js
@@ -0,0 +1,33 @@
+'use strict';
+function Utils() {
+}
+;
+Utils.prototype.max = function(objList, comparisionField) {
+    var result = -Infinity,
+            maxSoFar = -Infinity;
+
+    objList.forEach(function(obj, index, arr) {
+        var fieldValue = obj[comparisionField];
+        if (fieldValue > maxSoFar) {
+            maxSoFar = fieldValue;
+            result = obj;
+        }
+    });
+
+    return result;
+};
+
+Utils.prototype.min = function(objList, comparisionField) {
+    var result = -Infinity,
+            minSoFar = +Infinity;
+
+    objList.forEach(function(obj, index, arr) {
+        var fieldValue = obj[comparisionField];
+        if (fieldValue < minSoFar) {
+            minSoFar = fieldValue;
+            result = obj;
+        }
+    });
+
+    return result;
+};
